2b42b4feadc042db4a385a8e9d7da34ac2d234f1
Quanley Marieatte <qmarieatte@hotmail.com>
2017-10-24 11:11:24 -0400

    Initial Commiit

---
+++ lab1.c
@@ -0,0 +1,22 @@
+//Quanley Marieatte - MicroP Lab 1
+//
+#include <p18f452.h>
+#include <stdio.h>
+#include <delays.h>
+
+/*Set configuration bits for use with PICKit3*/
+#pragma config OSC = HS
+#pragma config WDT = OFF
+#pragma config LVP = OFF
+
+int i = 0;
+
+
+void main (void){
+    TRISB = 0xF0;
+    PORTB = 0;
+    for (i = 0; i < 15 ; i++){
+        PORTB = i + 1;
+        Delay10KTCYx(100);
+    }
+}
